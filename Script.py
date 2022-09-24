637af0ae76309cbd626627e3a90db4b93322c0ab
mdboss2 <114162987+mdboss2@users.noreply.github.com>
2022-09-24 13:51:10 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,92 +1,88 @@
 class script(object):
-    START_TXT = """Hello {} 👨‍💻,
-My Name is <a href='https://t.me/SamanthaSearchBot'>Samantha Movies Bot</a>
-I Can Provide Movies In Telegram Groups. You Can Search Movies Via Inline. I Can Also Add Filters In Telegram Groups.
-Just Add Me To Your Group And Enjoy Of All Available Movies On TG.
-Made With ❤ BY @TeleRoidGroup"""
-    HELP_TXT = """Hell'O {}
-Here is the Help For My Bot Commands."""
-    ABOUT_TXT = """☞ My Name: Sam Movies
-☞ Developer: @TheTeleRoid
-☞ Library: Pyrogram
-☞ Language: Python 3
-☞ DataBase: Mongo DB
-☞ Bot Server: Heroku
-☞ Build Status: v1.0.1 [Beta]
-☞ Updates:<a href='https://t.me/TeleRoidGroup'>@𝚃𝚎𝚕𝚎𝚁𝚘𝚒𝚍𝙶𝚛𝚘𝚞𝚙</𝚊>
-☞ Support: <a href='https://t.me/TeleRoid14'> @𝚃𝚎𝚕𝚎𝚁𝚘𝚒𝚍14</𝚊>
-☞ BotsList" : <a href='https://t.me/TheTeleRoid'> @𝙿𝙷𝙻𝚒𝚜𝚝𝙱𝚘𝚝</𝚊>"""
+    START_TXT = """𝙷𝙴𝙻𝙾 {},
+𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a><i><b>I Can Provide Movies In Telegram Groups. You Can Search Movies Via Inline. I Can Also Add Filters In Telegram Groups.
+Just Add Me To Your Group And Enjoy Of All Available Movies On TG."""</b></i>
+    HELP_TXT = """𝙷𝙴𝚈 {}
+𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
+    ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/Knmlpro2>𝚁𝙰𝙰𝙳𝙷𝙰 𝙺𝚁𝙸𝚂𝙷𝙽𝙰</a>
+✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
+✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
+✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
+✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
+✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
-- Eva Maria is a open source project. 
-- Source - https://github.com/PredatorHackerzZ/SamanthaMoviesBot
+<b>- Raadha Krishna is a open source project. 
+- Powered by - @knmlpro2
+- Join Main Group - @knmlpro2_group</b>
 
 <b>DEVS:</b>
-- <a href=https://t.me/TheTeleRoid>Team TeleRoid</a>"""
+- <a href=https://t.me/knmlpro2>Team KNMLPRO2</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
-- Filter is the feature were users can set automated replies for a particular keyword and tessa will respond whenever a keyword is found the message
+- Filter is the feature were users can set automated replies for a particular keyword and Raadha Krishna will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
-1. Samantha maria should have admin privillage.
+<i>1. Raadha Krishna should have admin privillage.
 2. only admins can add filters in a chat.
-3. alert buttons have a limit of 64 characters.
+3. alert buttons have a limit of 64 characters.</i>
 
 <b>Commands and Usage:</b>
-• /filter - <code>add a filter in chat</code>
+<i>• /filter - <code>add a filter in chat</code>
 • /filters - <code>list all the filters of a chat</code>
 • /del - <code>delete a specific filter in chat</code>
-• /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
+• /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""</i>
     BUTTON_TXT = """Help: <b>Buttons</b>
 
-- Eva Maria Supports both url and alert inline buttons.
+- Raadha Krishna Supports both url and alert inline buttons.
 
 <b>NOTE:</b>
-1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. Eva Maria supports buttons with any telegram media type.
-3. Buttons should be properly parsed as markdown format
+<i>1. Telegram will not allows you to send buttons without any content, so content is mandatory.
+2. Raadha Krishna supports buttons with any telegram media type.
+3. Buttons should be properly parsed as markdown format</i>
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https//t.me/EvaMariaBot)</code>
+<code>[Button Text](buttonurl:https://t.me/Knmlpro2Filter_Bot)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
     AUTOFILTER_TXT = """Help: <b>Auto Filter</b>
 
 <b>NOTE:</b>
-1. Make me the admin of your channel if it's private.
+<i>1. Make me the admin of your channel if it's private.
 2. make sure that your channel does not contains cam rip, porn and fake files.
 3. Forward the last message to me with quotes.
- I'll add all the files in that channel to my db."""
+ I'll add all the files in that channel to my db."""</i>
     CONNECTION_TXT = """Help: <b>Connections</b>
 
-- Used to connect bot to PM for managing filters 
-- it helps to avoid spamming in groups.
+<i>- Used to connect bot to PM for managing filters 
+- it helps to avoid spamming in groups.</i>
 
 <b>NOTE:</b>
-1. Only admins can add a connection.
-2. Send <code>/connect</code> for connecting me to ur PM
+<i>1. Only admins can add a connection.
+2. Send <code>/connect</code> for connecting me to ur PM</i>
 
 <b>Commands and Usage:</b>
-• /connect  - <code>connect a particular chat to your PM</code>
+<i>• /connect  - <code>connect a particular chat to your PM</code>
 • /disconnect  - <code>disconnect from a chat</code>
-• /connections - <code>list all your connections</code>"""
+• /connections - <code>list all your connections</code>"""</i>
     EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
 
 <b>NOTE:</b>
 these are the extra features of tessa
 
 <b>Commands and Usage:</b>
-• /id - <code>get id of a specifed user.</code>
+<i>• /id - <code>get id of a specifed user.</code>
 • /info  - <code>get information about a user.</code>
 • /imdb  - <code>get the film information from IMDb source.</code>
-• /search  - <code>get the film information from various sources.</code>"""
+• /search  - <code>get the film information from various sources.</code>"""</i>
     ADMIN_TXT = """Help: <b>Admin mods</b>
 
 <b>NOTE:</b>
-This module only works for my admins
+<b>This module only works for my admins</b>
 
 <b>Commands and Usage:</b>
-• /logs - <code>to get the rescent errors</code>
+<i>• /logs - <code>to get the rescent errors</code>
 • /stats - <code>to get status of files in db.</code>
 • /users - <code>to get list of my users and ids.</code>
 • /chats - <code>to get list of the my chats and ids </code>
@@ -95,12 +91,12 @@ This module only works for my admins
 • /ban  - <code>to ban a user.</code>
 • /unban  - <code>to unban a user.</code>
 • /channel - <code>to get list of total connected channels</code>
-• /broadcast - <code>to broadcast a message to all users</code>"""
-    STATUS_TXT = """✪ Total Medias: <code>{}</code>
-✪ Total Users: <code>{}</code>
-✪ Total Chats: <code>{}</code>
-✪ Used Storage: <code>{}</code> 𝙼𝚒𝙱
-✪ Free Storage: <code>{}</code> 𝙼𝚒𝙱"""
+• /broadcast - <code>to broadcast a message to all users</code>"""</i>
+    STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
+★ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
+★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
+★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
+★ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱"""
     LOG_TEXT_G = """#NewGroup
 Group = {}(<code>{}</code>)
 Total Members = <code>{}</code>
